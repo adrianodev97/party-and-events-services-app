08d069f7a477968cb6b354adf0fd01cd13336766
Adriano Junior <adrianojunior.dev97@gmail.com>
2023-06-24 23:38:39 -0300

    remove unused style and rename styled function

--- src/style/global.ts
+++ src/style/global.ts
@@ -1,10 +1,6 @@
 import { styled } from '@mui/material'
 
-export const MUIStyledMain = styled('main')(({ theme }) => ({
-  width: '100%',
-}))
-
-export const MUIGlobalContainer = styled('div')(({ theme }) => ({
+export const MUIPageContainer = styled('div')(({ theme }) => ({
   maxWidth: '1232px',
   margin: '0 auto',
   padding: '0 1rem',
