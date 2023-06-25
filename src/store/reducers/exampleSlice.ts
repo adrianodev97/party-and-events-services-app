2640fb61800c43802cd68279d156b93ac20c1de0
Adriano Junior <adrianojunior.dev97@gmail.com>
2023-06-24 23:02:10 -0300

    adjust store and slice

--- src/store/reducers/exampleSlice.ts
+++ src/store/reducers/exampleSlice.ts
@@ -2,15 +2,19 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { IExample } from './types'
 
 const initialState: IExample = {
-  name: '',
+  id: '',
+  email: '',
+  isLogged: false,
 }
 
 const exampleSlice = createSlice({
-  name: 'contactForm',
+  name: 'userAuth',
   initialState,
   reducers: {
     getExampleName(state, action: PayloadAction<IExample>) {
-      state.name = action.payload.name
+      state.id = action.payload.id
+      state.email = action.payload.email
+      state.isLogged = action.payload.isLogged
     },
   },
 })
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import exampleReducer from './reducers/exampleSlice'
 
-export const storeRT = configureStore({
+export const store = configureStore({
   reducer: {
     example: exampleReducer,
   },
 })
 
-export type RootState = ReturnType<typeof storeRT.getState>
-export type AppDispatch = typeof storeRT.dispatch
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
