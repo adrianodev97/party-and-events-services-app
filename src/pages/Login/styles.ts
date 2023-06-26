fd63274d40e27171182110282f9ea5773d27cdc7
Adriano Junior <adrianojunior.dev97@gmail.com>
2023-06-25 23:52:27 -0300

    add login page styles

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,3 +1,30 @@
 import { styled } from '@mui/material'
+import Image from 'next/image'
 
-export const MUIStyledElement = styled('div')(({ theme }) => ({}))
+export const MUIStyledLoginSeciton = styled('section')(({ theme }) => ({}))
+
+export const MUIStyledLoginBlock = styled('div')(({ theme }) => ({}))
+
+export const MUIStyledSignInForm = styled('div')(({ theme }) => ({}))
+
+export const MUIStyledSignUpForm = styled('div')(({ theme }) => ({}))
+
+export const MUIStyledLogoBox = styled('div')(({ theme }) => ({}))
+
+export const MUIStyledlogo = styled(Image)(({ theme }) => ({}))
+
+export const MUIStyledHaveAccountTitle = styled('h2')(({ theme }) => ({}))
+
+export const MUIStyledSignInBox = styled('div')(({ theme }) => ({}))
+
+export const MUIStyledSignUpBox = styled('div')(({ theme }) => ({}))
+
+export const MUIStyledInfoBoxBg = styled('div')(({ theme }) => ({}))
+
+export const MUIStyledFormBox = styled('div')(({ theme }) => ({}))
+
+export const MUIStyledFormSignIn = styled('div')(({ theme }) => ({}))
+
+export const MUIStyledFormSignUp = styled('div')(({ theme }) => ({}))
+
+export const MUIStyledFormTitle = styled('h3')(({ theme }) => ({}))
