4be08e5b7c6cfcd85d815359f478b207478e8f9a
Adriano Junior <adrianojunior.dev97@gmail.com>
2023-09-11 12:56:41 -0300

    refactor: change container name

--- src/style/global.ts
+++ src/style/global.ts
@@ -1,6 +1,6 @@
 import { styled } from '@mui/material'
 
-export const MUIPageContainer = styled('div')(({ theme }) => ({
+export const MUIGlobalContainer = styled('div')(({ theme }) => ({
   maxWidth: '1232px',
   margin: '0 auto',
   padding: '0 1rem',
